34fa126cab986bb0b844e676c4ab523fd215f11e
Ryan Liu <ryanzhliu@gmail.com>
2019-11-15 12:42:51 -0500

    Uncommented commands in setup.sh

--- kube-cluster-setup/setup.sh
+++ kube-cluster-setup/setup.sh
@@ -1,3 +1,3 @@
-# ansible-playbook -i hosts --ask-pass ./playbooks/initial.yml --user $1
-# ansible-playbook -i hosts --ask-pass ./playbooks/master.yml --user $1
+ansible-playbook -i hosts --ask-pass ./playbooks/initial.yml --user $1
+ansible-playbook -i hosts --ask-pass ./playbooks/master.yml --user $1
 ansible-playbook -i hosts --ask-pass ./playbooks/workers.yml --user $1
