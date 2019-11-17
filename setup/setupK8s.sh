31c55efe88839cd4bdd9aff1abb2d509ef2ffd91
Ryan Liu <ryanzhliu@gmail.com>
2019-11-16 22:04:25 -0500

    Updated k8s setup plays to also setup local kubectl to connect to master (i.e. no need to ssh to master anymore)

--- setup/setupK8s.sh
+++ setup/setupK8s.sh
@@ -1,3 +1,3 @@
-ansible-playbook -i hosts --ask-pass ./k8s/initial.yml --user $1
-ansible-playbook -i hosts --ask-pass ./k8s/master.yml --user $1
-ansible-playbook -i hosts --ask-pass ./k8s/workers.yml --user $1
+# playbooks based on https://www.digitalocean.com/community/tutorials/how-to-create-a-kubernetes-cluster-using-kubeadm-on-ubuntu-18-04
+
+ansible-playbook -i hosts --ask-pass ./k8s/main.yml --user $1
